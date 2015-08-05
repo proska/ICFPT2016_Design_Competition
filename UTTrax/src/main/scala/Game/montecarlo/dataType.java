df32367ae35686920588f7ccc24bb20728725d1e
saina <saina.h72@gamil.com>
2015-08-05 12:18:06 +0430

    data type.java is added

---
+++ UTTrax/src/main/scala/Game/montecarlo/dataType.java
@@ -0,0 +1,51 @@
+package Game.montecarlo;
+
+import Game.World.Move;
+import Game.World.gameState;
+
+import java.util.Vector;
+
+/**
+ * Created by saina on 8/5/15.
+ */
+class stateMC {
+    stateMC(){
+    }
+
+    stateMC(gameState st){
+
+        pegah = gameState.apply(st);
+
+        parent = null;
+        children = new Vector<stateMC>();
+        is_marked = 0;
+
+        score = 0;
+        visit = 0;
+        move = null;
+    }
+
+    stateMC(stateMC p){
+        parent = p;
+    }
+    stateMC parent;
+    Vector<stateMC> children;
+    int is_marked;
+    int score;
+    int visit;
+//    boolean win;
+    Move move;
+    gameState pegah;
+
+}
+
+class coordinate{
+    int x;
+    int y;
+    int dir;
+
+}
+class Rout{
+    coordinate start;
+    coordinate end;
+}
